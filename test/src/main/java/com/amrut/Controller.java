380ab92c52e38f543724cea9e200f3c454d7cff6
admin <admin@admin-PC>
2018-11-25 15:38:25 +0530

    developer 2 message

--- test/src/main/java/com/amrut/Controller.java
+++ test/src/main/java/com/amrut/Controller.java
@@ -14,7 +14,7 @@ public class Controller {
 	//method to change name, removed iterator using other developer for loop
 	public void changeName(){
 				
-		System.out.println("this is slave branch from developer 1");
+		System.out.println("this is slave branch from developer 2");
 		names.put("1", "amrut");
 		names.put("2", "rohit");
 		names.put("3", "rohan");
