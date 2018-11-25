426b99b029d978b0cccd6cf64772eea350632a94
admin <admin@admin-PC>
2018-11-25 12:38:18 +0530

    map looping

--- test/src/main/java/com/amrut/Controller.java
+++ test/src/main/java/com/amrut/Controller.java
@@ -1,6 +1,8 @@
 package com.amrut;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 
 public class Controller {
@@ -10,9 +12,19 @@ public class Controller {
 
 	//method to change name
 	public void changeName(){
+		
+		System.out.println("inside method changeName");
 		names.put("1", "amrut");
 		names.put("2", "rohit");
 		names.put("3", "rohan");
 		names.put("4", "rahul");
+		
+		Set<String> keys=names.keySet();
+		Iterator<String>it=keys.iterator();
+		
+		while(it.hasNext()){
+			String key=it.next();
+			System.out.println(key+ "  "+names.get(key) );
+		}
 	}
 }
