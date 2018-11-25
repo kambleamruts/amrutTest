cc7054a405c0bd60cf05f8d2137f4dc013da3e0f
admin <admin@admin-PC>
2018-11-25 18:45:11 +0530

    developer 1

--- test/src/main/java/com/amrut/Controller.java
+++ test/src/main/java/com/amrut/Controller.java
@@ -15,7 +15,7 @@ public class Controller {
 	//method to change name, removed iterator using other developer for loop
 	public void changeName(){		
 
-		System.out.println("this is master   branch");
+		System.out.println("this is master   branch by developer 1");
 		names.put("1", "amrut");
 		names.put("2", "rohit");
 		names.put("3", "rohan");
