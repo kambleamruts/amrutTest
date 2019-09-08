3be396d706ef47b294f4255a309b85796dce9594
admin <admin@admin-PC>
2019-09-08 20:50:36 +0530

    chnaged askconstant

--- test/src/main/java/com/amrut/AskConstant.java
+++ test/src/main/java/com/amrut/AskConstant.java
@@ -1,5 +1,7 @@
 package com.amrut;
 
+import com.ibm.mq.jms.MQQueueConnectionFactory;
+
 public class AskConstant {
 
 	
@@ -7,4 +9,11 @@ public class AskConstant {
 	public final static String EMPLOYEE_ID="12345";
 	public final static String EMPLOYEE_SALARY="34.34";
 	public final static String EMPLOYEE_DOB="24/10/1989";
+	
+	MQQueueConnectionFactory asdsa=new MQQueueConnectionFactory();
+	
+	public void show(){
+		asdsa.setHostName("");
+		
+	}
 }
