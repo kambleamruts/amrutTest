36b81be80ab5930ce09e44103e6ee4b35bb464a0
Amrut <Amrut@DESKTOP-A1FKTNJ>
2019-11-16 14:10:58 +0530

    revert commit 3

--- test/src/main/java/com/amrut/Controller.java
+++ test/src/main/java/com/amrut/Controller.java
@@ -26,11 +26,13 @@ public class Controller {
 			System.out.println(name+ "  "+names.get(name) );
 
 		System.out.println("done with changes");
-		
-		
-		
-	
+		List<String> ids=new ArrayList<String>();
+		ids.add("1");
+		ids.add("2");
+		ids.add("3");
+		ids.add("4");
+		ids.add("5");
+		ids.add("6");
 
-		
 	}
 }
